708d79a768f407bf1ec5f2aa92ae408dfdf3da1e
dhanush.k.mariappan <dhanush.k.mariappan@CDC2-L-F2ALD75.dir.svc.accenture.com>
2023-08-15 23:42:05 +0530

    Added to scan Base packages

--- Demo10010_Spring_Boot_Mechanical_Sympo/src/main/java/com/acgcetmechfest/Demo10010SpringBootMechanicalSympoApplication.java
+++ Demo10010_Spring_Boot_Mechanical_Sympo/src/main/java/com/acgcetmechfest/Demo10010SpringBootMechanicalSympoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.acgcetmechfest")
 public class Demo10010SpringBootMechanicalSympoApplication {
 
 	public static void main(String[] args) {
