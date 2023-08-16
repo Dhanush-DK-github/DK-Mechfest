acffd91d82171723d2004a7eafa8439a3c96b236
dhanush.k.mariappan <dhanush.k.mariappan@CDC2-L-F2ALD75.dir.svc.accenture.com>
2023-08-16 11:16:18 +0530

    WebPage Path Change Try-10

--- Demo10010_Spring_Boot_Mechanical_Sympo/src/main/java/com/acgcetmechfest/Demo10010SpringBootMechanicalSympoApplication.java
+++ Demo10010_Spring_Boot_Mechanical_Sympo/src/main/java/com/acgcetmechfest/Demo10010SpringBootMechanicalSympoApplication.java
@@ -2,15 +2,26 @@ package com.acgcetmechfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.acgcetmechfest.entity.Suggestion;
+
 @SpringBootApplication(scanBasePackages = "com.acgcetmechfest")
-@EnableWebMvc
 public class Demo10010SpringBootMechanicalSympoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo10010SpringBootMechanicalSympoApplication.class, args);
 		System.out.println("Hii");
+	}
+	
+	@GetMapping(value = {"/", "/home"})
+	public String showHomePage() {
+		System.out.println("hello");
+		
+		return "home";
 	}
 
 }
