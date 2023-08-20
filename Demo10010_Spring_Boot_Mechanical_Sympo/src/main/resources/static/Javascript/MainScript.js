5d867019147f5416519069eb7e3aee9a256ef3f9
dhanush.k.mariappan <dhanush.k.mariappan@CDC2-L-F2ALD75.dir.svc.accenture.com>
2023-08-20 21:16:30 +0530

    Refresh Config Update

--- Demo10010_Spring_Boot_Mechanical_Sympo/src/main/resources/static/Javascript/MainScript.js
+++ Demo10010_Spring_Boot_Mechanical_Sympo/src/main/resources/static/Javascript/MainScript.js
@@ -11,12 +11,11 @@
             document.onkeypress = timerReset;
 
             function timerElapsed() {
-                console.log("Timer elapsed");
                 location.reload();
             };
 
             function timerReset() {
-                console.log("Reseting timer");
+
                 clearTimeout(timer);
                 timer = setTimeout(timerElapsed, 5 * 60 * 1000); // 5 mins
             }
